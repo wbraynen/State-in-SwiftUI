55f873afb19845fd3dc921289c35e8db49fc7f8b
Will Braynen <wbraynen@gmail.com>
2024-03-21 18:41:43 -0400

    Initial Commit

---
+++ TestApp/TestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestAppApp.swift
+//  TestApp
+//
+//  Created by Will on 3/21/24.
+//
+
+import SwiftUI
+
+@main
+struct TestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
